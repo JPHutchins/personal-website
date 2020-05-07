fa782f9a1c69fb51b1586f3d5cf040aa0ce83f94
J.P. Hutchins <jphutchins@gmail.com>
2020-05-06 22:01:02 -0700

    Improve load performance from those darn CDNs

--- script.js
+++ script.js
@@ -25,6 +25,10 @@ contents.forEach((item) => {
     current.content.classList.add("hidden");
     current.content = content;
     current.content.classList.remove("hidden");
+
+    if (item === "music") {
+        loadListeningPage();
+    }
   };
 
   content.querySelectorAll(".details").forEach((node) => {
@@ -43,4 +47,44 @@ contents.forEach((item) => {
       more.innerText = "Less...";
     };
   });
+
+  
 });
+
+const embedClassical = 
+  `
+<iframe width="100%" height="20" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/12641491&color=%23ff5500&inverse=false&auto_play=false&show_user=true"></iframe>
+<iframe width="100%" height="20" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/12641405&color=%23ff5500&inverse=false&auto_play=false&show_user=true"></iframe>
+<iframe width="100%" height="20" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/12641369&color=%23ff5500&inverse=false&auto_play=false&show_user=true"></iframe>
+<iframe width="100%" height="20" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/12641128&color=%23ff5500&inverse=false&auto_play=false&show_user=true"></iframe>
+<iframe width="100%" height="20" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/12640972&color=%23ff5500&inverse=false&auto_play=false&show_user=true"></iframe>
+`
+;
+
+embedClassicalNode = document.getElementById("embed-classical");
+
+const embedElectronic = 
+  `
+<iframe width="100%" height="20" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/57691802&color=%23ff5500&inverse=false&auto_play=false&show_user=true"></iframe>
+<iframe width="100%" height="20" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/21071956&color=%23ff5500&inverse=false&auto_play=false&show_user=true"></iframe>
+<iframe width="100%" height="20" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/110637749&color=%23ff5500&inverse=false&auto_play=false&show_user=true"></iframe>
+<iframe width="100%" height="20" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/272643741&color=%23ff5500&inverse=false&auto_play=false&show_user=true"></iframe>
+<iframe width="100%" height="20" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/176465990&color=%23ff5500&inverse=false&auto_play=false&show_user=true"></iframe>
+<iframe width="100%" height="20" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/46074877&color=%23ff5500&inverse=false&auto_play=false&show_user=true"></iframe>
+<iframe width="100%" height="20" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/47042667&color=%23ff5500&inverse=false&auto_play=false&show_user=true"></iframe>
+<iframe width="100%" height="20" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/21071635&color=%23ff5500&inverse=false&auto_play=false&show_user=true"></iframe>
+<iframe width="100%" height="20" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/193220882&color=%23ff5500&inverse=false&auto_play=false&show_user=true"></iframe>
+<iframe width="100%" height="20" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/176940031&color=%23ff5500&inverse=false&auto_play=false&show_user=true"></iframe>
+<iframe width="100%" height="20" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/155008771&color=%23ff5500&inverse=false&auto_play=false&show_user=true"></iframe>
+<iframe width="100%" height="20" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/15753167&color=%23ff5500&inverse=false&auto_play=false&show_user=true"></iframe>
+<iframe style="border: 0; width: 100%; height: 42px;" src="https://bandcamp.com/EmbeddedPlayer/album=524206945/size=small/bgcol=ffffff/linkcol=0687f5/track=3234460618/transparent=true/" seamless><a href="http://ravertoothtiger.bandcamp.com/album/year-of-the-ravertooth-vol-1">Year Of The Ravertooth Vol.1 by Lucider</a></iframe>    
+`
+;
+
+embedElectronicNode = document.getElementById("embed-electronic");
+
+const loadListeningPage = () => {
+  if (document.getElementById("embed-electronic").innerHTML) return;
+  embedClassicalNode.innerHTML = embedClassical;
+  embedElectronicNode.innerHTML = embedElectronic;
+};
